a8ca90361dfdaf40a47333be8bd842b9042abcee
wesolyromek <gracz1500@gmail.com>
2014-03-22 21:42:39 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 debug-console
 =============
 
-A togglable interactive mode for node.js
+A simple togglable interactive mode for node.js
+You can start it and stop from any place in your code.
+
+There are two functions available:
+
+* ```start([name]);```
+    * Starts interactive mode
+    * If name is specified it's printed at the beginning of interactive session
+* ```stop();```
+    * Stops interactive mode
+
+To quit type ```:q```
+
+Example usage:
+
+```javascript
+var interactive = require('debug-console');
+interactive.start('Point 1');
+
+//Interactive mode started at: Point 1
+//>
+```
+
+Licensed under MIT license. Copyright (c) 2014 Adam Paszke
---
+++ debug-console.js
@@ -0,0 +1,229 @@
+var util = require('util');
+var stdin = process.stdin;
+var stdout = process.stdout;
+
+
+
+//COMMAND VARIABLES
+var command_memory = [];
+var tmp = "";
+var index = 0;
+var pos = 0;
+var display = "";
+var exit = false;
+var indentation = 0;
+var saved = "";
+
+var refreshLine = function () {
+    display = display.replace(/[\x00-\x1F\x7F-\x9F]/, "", "g");
+    var offset = display.length-pos+1;
+    stdout.write('\u001b[2K\r');
+    if(indentation === 0) {
+        stdout.write('> ');
+    }
+    for(var i = 0; i < indentation; i++) {
+        stdout.write('... ');
+    }
+    stdout.write(display + ' \u001b['+offset+'D');
+    if(index == command_memory.length) {
+        tmp = display;
+    }
+    exit = false;
+};
+
+var countindentationation = function (line) {
+    var len = line.length;
+    for(var i = 0; i < len; i++) {
+        if(line[i] == '{') {
+            indentation++;
+        } else if(line[i] == '}') {
+            indentation--;
+        }
+    }
+};
+
+var processKey = function( key ){
+    switch (key) {
+        /*---------------------------------------------/
+        /--------------------CTRL+C--------------------/
+        /---------------------------------------------*/
+        case '\u0003':
+            if(!exit) {
+                stdout.write('\n(^C again to quit)\n> ');
+                exit = true;
+            } else {
+                stdout.write('\n');
+                process.exit();
+            }
+            break;
+        /*---------------------------------------------/
+        /--------------------CTRL+L--------------------/
+        /---------------------------------------------*/
+        case '\u000c':
+            stdout.write('\u001b[2J\u001b[H');
+            refreshLine();
+            break;
+        /*---------------------------------------------/
+        /--------------------ENTER---------------------/
+        /---------------------------------------------*/
+        case '\u000d':
+            var result;
+            
+            countindentationation(display);
+            
+            if(display == ':q') {
+                stdout.write('\n\u001b[37m' + 'Interactive mode off' + '\u001b[0m\n');
+                stdin.removeListener('data', processKey);
+                stdin.setRawMode(false);
+                break;
+            }
+            
+            if(display.length === 0) {
+                if(saved.length === 0) {
+                    stdout.write('\n\u001b[37mundefined\u001b[0m');
+                }
+                pos = 0;
+                tmp = "";
+                display = "";
+                refreshLine();
+                break;
+            }
+            
+            saved += display;
+            command_memory.push(display);
+            
+            if(indentation === 0) {
+                try {
+                    stdout.write('\n');
+                    result = eval(saved);
+                    stdout.write('\u001b[37m' + util.inspect(result) + '\u001b[0m');
+                } catch (e) {
+                    stdout.write(e.toString() + e.stack);
+                } finally {
+                    saved = "";
+                }
+
+                index = command_memory.length;
+                if(command_memory.length > 100) {
+                    command_memory.shift();
+                }
+            }
+            
+            tmp = "";
+            display = "";
+            pos = 0;
+            stdout.write('\n');
+            refreshLine();
+            break;
+        /*---------------------------------------------/
+        /-------------------BACKSPACE------------------/
+        /---------------------------------------------*/
+        case '\u007F':
+            if(pos > 0) {
+                display = display.substr(0, pos-1) + display.substr(pos);
+                pos--;
+                refreshLine();
+            }
+            break;
+        /*---------------------------------------------/
+        /-------------------UP ARROW-------------------/
+        /---------------------------------------------*/
+        case '\u001b[A':
+            if(command_memory[index-1]) {
+                index--;
+                display = command_memory[index];
+                pos = display.length;
+                refreshLine();
+            }
+            break;
+        /*---------------------------------------------/
+        /------------------DOWN ARROW------------------/
+        /---------------------------------------------*/
+        case '\u001b[B':
+            if(command_memory[index+1]) {
+                index++;
+                display = command_memory[index];
+                pos = display.length;
+                refreshLine();
+            } else {
+                index = command_memory.length;
+                pos = tmp.length;
+                display = tmp;
+                refreshLine();
+            }
+            break;
+        /*---------------------------------------------/
+        /------------------LEFT ARROW------------------/
+        /---------------------------------------------*/
+        case '\u001b[D':
+            if(pos > 0) {
+                stdout.write('\u001b[1D');
+                pos--;
+            }
+            break;
+        /*---------------------------------------------/
+        /-----------------RIGHT ARROW------------------/
+        /---------------------------------------------*/
+        case '\u001b[C':
+            if (pos < display.length) {
+                stdout.write('\u001b[1C');
+                pos++;
+            }
+            break;
+        /*---------------------------------------------/
+        /---------------------HOME---------------------/
+        /---------------------------------------------*/
+        case '\u001bOH':
+            pos = 0;
+            refreshLine();
+            break;
+        /*---------------------------------------------/
+        /---------------------END----------------------/
+        /---------------------------------------------*/
+        case '\u001bOF':
+            pos = display.length;
+            refreshLine();
+            break;
+        /*---------------------------------------------/
+        /-------------------ALL KEYS-------------------/
+        /---------------------------------------------*/
+        default:
+            if(pos == display.length) {
+                display += key;
+                pos = display.length;
+            } else if (pos < display.length) {
+                display = display.substr(0, pos) + key + display.substr(pos);
+                pos++;
+            }
+            refreshLine();
+    }
+};
+
+/*---------------------------------------------/
+/--------------------START---------------------/
+/---------------------------------------------*/
+module.exports.start = function (name) {
+    if(name) {
+        stdout.write('Interactive mode started at: ' + name + '\n');
+    }
+    stdin.setRawMode(true);
+    stdin.setEncoding('utf8');
+    stdout.setEncoding('utf8');
+    stdin.resume();
+    stdin.on('data', processKey);
+    command_memory = [];
+    tmp = "";
+    index = 0;
+    pos = 0;
+    display = "";
+    exit = false;
+    indentation = 0;
+    saved = "";
+    refreshLine();
+};
+
+module.exports.stop = function () {
+    stdout.write('\u001b[37m' + 'Interactive mode off\n' + '\u001b[0m');
+    stdin.removeListener('data', processKey);
+    stdin.setRawMode(false);
+};
